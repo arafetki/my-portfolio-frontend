f7fd2fe6be86a61073dcb3b563e1ba27d8a5a13b
Arafet Ben Kilani <arafet.benkilani@supcom.tn>
2024-02-18 12:21:19 +0100

    Project scafold

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>My Personal Portfolio Website V1</h1>
+      <div className="card">
+        <p>Under Construction</p>
+      </div>
+    </>
+  )
+}
+
+export default App
